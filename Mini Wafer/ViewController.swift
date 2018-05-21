5dcb09ae106d3795256c016418657391201e5de5
Tolu Oluwagbemi <tulburg2@gmail.com>
2018-05-21 23:13:39 +0100

    Initial Commit

---
+++ Mini Wafer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mini Wafer
+//
+//  Created by tolu oluwagbemi on 5/21/18.
+//  Copyright © 2018 tolu oluwagbemi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
